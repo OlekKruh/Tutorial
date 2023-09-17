934e61725382de6b1f171de7370321782eeb967a
OlekKruh <ksalexks@gmail.com>
2023-09-17 16:59:10 +0200

    Initial commit

--- AutoChecker.py
+++ AutoChecker.py
@@ -1,13 +1,9 @@
 import re
 
 
-def find_word(text, word):
-    res_dict = {}
-    pattern = re.compile(word, flags=re.IGNORECASE)
-    result = re.findall(pattern, text)
+def replace_spam_words(text, spam_words):
+    for word in spam_words:
+        pattern = re.compile(r'\b' + re.escape(word) + r'\b', flags=re.IGNORECASE)
+
+        result = pattern.sub('***', text)
     return result
-
-
-print(find_word(
-    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
-    "Python"))
