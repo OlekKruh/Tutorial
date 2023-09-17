f88635263ab532f6fc6280346f5a344a6f33f28b
OlekKruh <ksalexks@gmail.com>
2023-09-17 22:31:40 +0200

    Initial commit

--- AutoChecker.py
+++ AutoChecker.py
@@ -2,12 +2,10 @@ import re
 
 
 def find_all_emails(text):
-    pattern = re.compile(r'\b[A-Za-z\d_.]{2,}@[A-Za-z]+\.[A-Za-z]{2,}\b')
+    pattern = re.compile(r'\b[a-zA-Z][a-zA-Z\d_.]+@[a-zA-Z]+\.[a-zA-Z_]{2,}\b')
     match = pattern.findall(text)
+    print(match)
+    return match
 
-    mail_list = []
 
-    for i in match:
-        mail_list.append(i)
-    print(mail_list)
-    return mail_list
+print(find_all_emails('Ima.Fool@iana.org Ima.Fool@iana.o 1Fool@iana.org first_last@iana.org first.middle.last@iana.or a@test.com abc111@test.com.net'))
