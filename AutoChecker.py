d6dbc37c2ea380a6de8d8fe6add175340e1ce518
OlekKruh <ksalexks@gmail.com>
2023-09-17 17:17:49 +0200

    Initial commit

--- AutoChecker.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-def replace_spam_words(text, spam_words):
-    for word in spam_words:
-        pattern = re.compile(r'\b' + re.escape(word) + r'\b', flags=re.IGNORECASE)
-
-        result = pattern.sub('***', text)
-    return result
