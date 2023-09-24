59ae615101525dcf7917882a5bce52fc7dfe1af7
OlekKruh <ksalexks@gmail.com>
2023-09-24 15:31:40 +0200

    Initial commit

--- AutoChecker.py
+++ AutoChecker.py
@@ -1,11 +1,5 @@
-import re
+def write_employees_to_file(employee_list, path):
 
 
-def find_all_emails(text):
-    pattern = re.compile(r'\b[a-zA-Z][a-zA-Z\d_.]+@[a-zA-Z]+\.[a-zA-Z_]{2,}\b')
-    match = pattern.findall(text)
-    print(match)
-    return match
 
-
-print(find_all_emails('Ima.Fool@iana.org Ima.Fool@iana.o 1Fool@iana.org first_last@iana.org first.middle.last@iana.or a@test.com abc111@test.com.net'))
+print(total_salary('list.txt'))
