164351e202cb135eceaef2d469df96c0039cc925
OlekKruh <ksalexks@gmail.com>
2023-09-25 19:04:16 +0200

    Initial commit

--- AutoChecker.py
+++ AutoChecker.py
@@ -1,5 +1,17 @@
-def write_employees_to_file(employee_list, path):
+def get_cats_info(path):
+    list_of_cats = []
+    with open(path, 'r') as content:
+        while True:
+            data = content.readline().strip()
+            if not data:
+                break
+            line = data.split(',')
+            client = {
+                'id': line[0],
+                'name': line[1],
+                'age': line[2]
+            }
+            list_of_cats.append(client)
+    return list_of_cats
 
-
-
-print(total_salary('list.txt'))
+print(get_cats_info('list.txt'))
