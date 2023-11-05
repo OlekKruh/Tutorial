16635c21084fb348130e6ce91cc1aa46c9642c7d
OlekKruh <ksalexks@gmail.com>
2023-11-05 21:58:41 +0100

    Initial commit

--- AutoChecker.py
+++ AutoChecker.py
@@ -1,13 +1,39 @@
-def to_lowercase_decorator(func):
-    def wrapper():
-        user_input = input("Введите текст: ")
-        lower_case_input = user_input.lower()
-        func(lower_case_input)
+import sys
+def input_error(func):
+    def wrapper(contact_list):
+        while True:
+            data = input(f'>>> ')
+            if len(data.split()) != 2:
+                print('Invalid input. Please enter both name and phone number.')
+                continue
+
+            name, phone = data.split()
+            if not name.isalpha():
+                print('Invalid input. Name should only contain letters.')
+                continue
+            if not phone.isdigit():
+                print('Invalid input. Phone number should only contain digits.')
+                continue
+
+            contact_list[name] = phone
+            print(f'Contact "{name}" with phone number "{phone}" added to the list.')
+            break
+
+        return contact_list
+
     return wrapper
 
-# Пример использования декоратора
-@to_lowercase_decorator
-def process_input(user_input):
-    print("Ваш текст в нижнем регистре:", user_input)
+@input_error
+def add(contact_list):
+    print(f'Please enter the data according to the specified sample after >>>\n'
+          f'Sample: John 123456789')
+    data = input(f'>>> ')
+    name, phone = data.split()
+    contact_list[name] = phone
+    print(f'Contact "{name}" with phone number "{phone}" added to the list.')
+    return contact_list
 
-process_input()
+
+contact_list = {}
+
+add(contact_list)
--- homework_09/Command Line Interface.py
+++ homework_09/Command Line Interface.py
@@ -1,18 +1,20 @@
 import sys
 import re
 
+
 # Error decorator
-def input_error(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except KeyError:
-            return 'Wrong contact name.'
-        except ValueError:
-            return 'Cannot convert data.'
-        except IndexError:
-            return 'No such contact in your contact list.'
-    return wrapper
+# def input_error(func):
+#     def wrapper(*args):
+#         try:
+#             return func(*args)
+#         except KeyError:
+#             return 'Wrong contact name.'
+#         except ValueError:
+#             return 'Cannot convert data.'
+#         except IndexError:
+#             return 'No such contact in your contact list.'
+#
+#     return wrapper
 
 
 # Exit program function
@@ -22,44 +24,45 @@ def exit_program():
     sys.exit()
 
 
-# All available commands output
-def help():
-    print("Available commands:")
-    print("1. hello - Replies on the console 'How can I help you?'")
-    print("2. add - Add a new name and phone number to your Notebook")
-    print("3. change - Changes the name and phone number of an existing contact")
-    print("4. phone - Display the phone number for a specific contact in the console.")
-    print("5. show all - Display all saved contacts with phone numbers in the console.")
-    print("6. good bye, close, exit, quit - Use any of these commands to exit the program.")
-    print("7. Do not use '.' in sentences; this will close the program immediately.")
+# Adding contacts
 
-
-def add(contact_list):
-    print(f'Please enter the data according to the specified sample after >>>\n'
-          f'Sample: John 123456789')
-    data = input(f'>>> ')
-    if re.match(r'^[A-Za-z]+\s\d{9}$', data):
-        name, phone = data.split()
-        contact_list[name] = phone
-        print(f'Contact "{name}" with phone number "{phone}" added to the list.')
-    else:
-
+def add(contact_list, name, phone):
+    contact_list[name] = phone
+    print(f'Contact "{name}" with phone number "{phone}" added to the list.\n')
     return contact_list
 
 
+def change(contact_list, name, phone):
+    if name in contact_list:
+        contact_list[name] = phone
+        print(f"Contact '{name}' updated with phone number '{phone}'.")
+        return contact_list
+    else:
+        return print(f"Contact '{name}' not found.")
+
+
+# Shoving content of Contact list
+
 def show_all(contact_list):
-    if not contact_list
+    if not contact_list:
         return 'Contact list is empty.'
     for key, value in contact_list.items():
         print(f'Name: {key}, Phone number: {value}')
 
 
+# Shoving contact phone number
+
+def phone(contact_list, name):
+    if name in contact_list:
+        return print(f"The phone number for '{name}' is {contact_list[name]}.")
+    else:
+        return print(f"Contact '{name}' not found.")
+
+
 COMMAND_LIST = {
-    'help': help,
-    'hello': lambda: "How can I help you?",
     'add': add,
-    # 'change': change,
-    # 'phone': phone,
+    'change': change,
+    'phone': phone,
     'show all': show_all,
     'good bye': exit_program,
     'close': exit_program,
@@ -69,24 +72,24 @@ COMMAND_LIST = {
     '.': exit_program,
 }
 
+contact_list = {}
+
 
 def main():
-    contact_list = {}
-    print_message = False
+    print(f'Expecting commend.\n')
     while True:
-        if not print_message:
-            print(f'Expecting commend.')
-            print_message = True
+        print(f'Available commands:\n'
+              f'-hello => Greetings\n'
+              f'-add [name] [phone number] => Adds a contact to the book\n'
+              f'-change [name] [phone number] => Changes a contact in a book\n'
+              f'-phone [name] => Shows contact number\n'
+              f'-show all => Shows all contacts numbers\n'
+              f'-good bye, close, exit, quit => Exiting the program\n')
 
-        command = input(f'>>> ').lower()
+        user_input = input('>>> ').lower().split()
+        print(user_input)
 
-        if command in COMMAND_LIST:
-            if command in ['add', 'change', 'phone', 'show all']:
-                contact_list = COMMAND_LIST[command](contact_list)
-            else:
-                COMMAND_LIST[command]()
-        else:
-            print('Command not recognized. Type "help" for a list of available commands.')
+
 
 
 if __name__ == '__main__':
