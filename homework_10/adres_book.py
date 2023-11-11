34e92e0f6c3d49f7c97439da238892e43c718520
OlekKruh <ksalexks@gmail.com>
2023-11-12 00:23:26 +0100

    Initial commit

--- homework_10/adres_book.py
+++ homework_10/adres_book.py
@@ -35,9 +35,8 @@ class Phone(Field):
 
 
 class Record:
-    def __init__(self, name, phone):
+    def __init__(self, name):
         self.name = Name(name)
-        self.phone = Phone(phone)
         self.phones = []
 
     # logika dodawania
@@ -80,3 +79,35 @@ class AddressBook(UserDict):
     def __str__(self):
         return "\n".join(str(record) for record in self.data.values())
 
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+for name, record in book.data.items():
+    print(record)
+
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+# Видалення запису Jane
+book.delete("Jane")
