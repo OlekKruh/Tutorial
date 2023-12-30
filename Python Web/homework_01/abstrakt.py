f4f9a720daf6652ad58e7c8ffd3d793b728cc048
OlekKruh <ksalexks@gmail.com>
2023-12-30 16:02:18 +0100

    Initial commit

--- Python Web/homework_01/abstrakt.py
+++ Python Web/homework_01/abstrakt.py
@@ -1,12 +1,29 @@
 from abc import ABC, abstractmethod
 
 
+COMMAND_LIST = {
+    'hello': lambda contact_list, *args: print("Hello. How can I help you?\n"),
+    'add': add_f,
+    'change': change_f,
+    'phone': phone_f,
+    'show all': show_all_f,
+    'good bye': exit_program_f,
+    'close': exit_program_f,
+    'exit': exit_program_f,
+    'quit': exit_program_f,
+    'qqq': exit_program_f,
+    '.': exit_program_f,
+}
+
+contact_list = {}
+
+
 class UI(ABC):
     def __init__(self):
         self.content = ""
 
     @abstractmethod
-    def display_content(self, content):
+    def display_content(self):
         pass
 
 
@@ -23,24 +40,23 @@ class UserInput(ABC):
         pass
 
 
-class DisplayContact(UI):
-    def display_content(self, content):
-        return (f'Name: {self._name}\n'
-                f'Phone: {", ".join(map(str, self._phone))}\n'
-                f'Birthday: {birthday_str}\n'
-                f'Days to next Birthday left: {self.days_to_birthday()}\n')
+class MenuUI(UI, UserInput):
+    def display_content(self):
+        separator = '='
+        quantity = 20
+        print(f'{separator * quantity}'
+              f'Maine Menu\n'
+              f'{separator * quantity}'
+              f'1. AddressBook\n'
+              f'2. NoteBook\n'
+              f'3. Exit\n'
+              f'{separator * quantity}')
 
-
-class DisplayNote(UI)
-    def display_content(self, content):
-        return (f'Title: {self._title}'
-                f'Tag: {", ".join(map(str, self._tag))}'
-                f'Text: {self._text}')
-
-
-class ConsoleUserInput(UserInput):
     def user_input(self, input_str):
-        self.input = input_str
-
-    def echo(self, input_str):
-        print(f"Echo: {input_str}")
+        for key in COMMAND_LIST.keys():
+            if input_str.startswith(key):
+                input_str = input_str.replace(key, '')
+                COMMAND_LIST[key](contact_list, *input_str.split())
+                break
+            else:
+                print(f'Unknown command. Try again.\n')
